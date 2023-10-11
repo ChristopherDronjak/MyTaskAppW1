0a71daeaa84712d916ed081cdaad3db2b84d60f3
ChristopherDronjak <yoschmoo@gmail.com>
2023-10-12 11:25:04 +1300

    MyTaskAppW1
    Task app developed in week 1 of term 4

---
+++ MauiApp1/MauiApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiApp1.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        IConnectivity connectivity;
+        public MainViewModel(IConnectivity connectivity)
+        {
+            Items = new ObservableCollection<string>();
+            this.connectivity = connectivity;
+        }
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+
+        async void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+
+            if(connectivity.NetworkAccess != NetworkAccess.Internet)
+            {
+                await Shell.Current.DisplayAlert("No Internet Connection!", "No Connectivity", "Try again");
+                return;
+            }
+
+            // adding items 
+            Items.Add(Text);
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string s)
+        {
+            if (Items.Contains(s))
+            {
+                Items.Remove(s);
+            }
+        }
+
+        [RelayCommand]
+        async Task Tap(string s)
+        {
+            await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+        }
+
+    }
+}
